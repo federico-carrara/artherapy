b501a0b86e6a8e6b66fef028bf13317b5b1f8045
federico-carrara <federico.carrara@fht.org>
2024-06-13 09:30:22 +0200

    Added script for setting up environment

---
+++ install.sh
@@ -0,0 +1,8 @@
+mamba create -n artherapy_env python=3.10
+mamba activate artherapy_env
+mamba install pandas -y
+mamba install scikit-learn -y
+mamba install matplotlib -y
+mamba install seaborn -y
+mamba install ipykernel -y
+pip install openpyxl
